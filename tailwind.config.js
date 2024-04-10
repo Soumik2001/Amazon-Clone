89f71a079070b5bab96281cff40da01265ab8eab
Soumik Mondal <soumikmondal61116@gmail.com>
2024-04-10 19:29:51 +0530

    Delete public folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  content: [
-    "./public/*.{html,js}",
-    "./node_modules/tw-elements/js/**/*.js"
-  ],
-  plugins: [require("tw-elements/plugin.cjs")],
-  darkMode: "class"
-};
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
